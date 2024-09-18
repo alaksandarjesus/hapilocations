6aff11b36a18c12d284e3f693577d91d24ff50d3
skriptxadmin <mailtoskriptx@gmail.com>
2024-09-18 10:58:37 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+** Routes **
+
+{{baseurl}} = https://hapilocations.skriptx.com/
+
+*** To get countries ***
+
+{{baseurl}}/countries
+
+*** To get states ***
+
+{{baseurl}}/states/{countryId}
+
+*** To get cities ***
+
+{{baseurl}}/cities/{stateId}
---
+++ app.js
@@ -0,0 +1,79 @@
+'use strict';
+const fs = require('fs');
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost',
+        routes: {
+            cors:true
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return '<a href="https://github.com/alaksandarjesus/hapilocations">For Details</a>';
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/countries',
+        handler: async (request, h) => {
+            try{
+                const countries = await fs.readFileSync('./locations/countries.min.json', {encoding:'utf-8'}) 
+                return JSON.stringify({success:true, countries:JSON.parse(countries)});
+          
+            }catch(e){
+                return JSON.stringify({success:false, countries:[]});
+
+            }
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/states/{id}',
+        handler: async (request, h) => {
+            try{
+                const states = await fs.readFileSync(`./locations/states/${request.params.id}.min.json`, {encoding:'utf-8'}) 
+                return JSON.stringify({success:true, states:JSON.parse(states)});
+          
+            }catch(e){
+                return JSON.stringify({success:false, states:[]});
+
+            }
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/cities/{id}',
+        handler: async (request, h) => {
+            try{
+                const cities = await fs.readFileSync(`./locations/cities/${request.params.id}.min.json`, {encoding:'utf-8'}) 
+                return JSON.stringify({success:true, cities:JSON.parse(cities)});
+          
+            }catch(e){
+                return JSON.stringify({success:false, cities:[]});
+
+            }
+        }
+    });
+
+    await server.start();
+    // console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
